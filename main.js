c9cc599fa353dbd3eb7ef72bec7e6d91230987e5
jeSuisCodeuse <creativegirl@outlook.fr>
2020-09-27 14:30:07 +0200

    auto writing text

---
+++ main.js
@@ -0,0 +1,14 @@
+const text = "Bonjour, Je m'appelle Samia, J'habite à Avignon, Je suis \"developpeuse\" web, Je travaille avec HTML, Css, Javascript, Je commence l'apprentissage du React, et le tout en autodidacte !... Contactez moi ;)";
+
+let begin = 0;
+
+function write() {
+    document.body.innerText = text.slice(0, begin);
+    begin++;
+
+    if (begin > text.length ) {
+        begin = 0 ;
+    }
+}
+
+setInterval(write, 300);
